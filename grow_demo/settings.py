aaf62058a8386a7a85f0a92523aa7d56313ec4d4
Abhishek Gupta <amansg710@gmail.com>
2026-06-06 04:07:10 +0530

    grow-invest

--- grow_demo/settings.py
+++ grow_demo/settings.py
@@ -87,11 +87,20 @@ USE_X_FORWARDED_HOST = True
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import os
 import dj_database_url
 
-DATABASES = {
-    "default": dj_database_url.config(conn_max_age=600, ssl_require=True)
-}
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.config(conn_max_age=600, ssl_require=True)
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
