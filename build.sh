2561a75a5e43d7c7c5b509a238f36dd5bb764e04
Abhishek Gupta <amansg710@gmail.com>
2026-06-07 03:13:08 +0530

    require

--- build.sh
+++ build.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Installing dependencies..."
-pip install -r requirements.txt
-
-echo "Creating static directory..."
-mkdir -p "$(dirname "$0")/static/css"
-
 echo "Running migrations..."
 python manage.py migrate --noinput
 
 echo "Collecting static files..."
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
 
 echo "Build complete"
